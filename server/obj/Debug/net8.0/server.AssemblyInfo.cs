f0c2ddc0d96e682759fa65b196cfba4485c989ae
Edwin Neoh <edwinneoh@gmail.com>
2025-10-21 16:07:44 +0800

    dotnet.yml updated

--- server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c48914dd5e5939788347c24794b77081a77d089")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d3122bae9bcd1b1327a9d31e276fbfd79ed3255")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
