5d1ae718b55b3edcc7529f265d1f7a94e9e9a2fc
tweiqin <qinqintay03@gmail.com>
2025-10-28 16:31:05 +0800

    Go

--- server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0c2ddc0d96e682759fa65b196cfba4485c989ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1f48ea26445450639d9bf8ebfcfbd796de6714c")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
