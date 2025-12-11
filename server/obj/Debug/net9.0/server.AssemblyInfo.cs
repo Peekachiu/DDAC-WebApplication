1145ea719b05dcc91f68a6862ef9d28c12e73489
Edwin Neoh <edwinneoh@gmail.com>
2025-12-11 08:31:09 +0800

    Cache Automatic Updates

--- server/obj/Debug/net9.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net9.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03dbec67662806878a6d4ac0ee053869d3cacec3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2b281c97f6971569eb5bef2313562a8d0c00db3")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
